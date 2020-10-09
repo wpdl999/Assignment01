846cdad3ae3a56bc6b10e467e0810dd60a5dd78c
Wooram Moon <wpdl555@naver.com>
2020-10-09 01:18:31 -0400

    Add assingment01

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+const app = require('../app');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' ,});
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' ,});
+});
+
+/* GET About Me page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' ,});
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' ,});
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' ,});
+});
+
+/* GET Contact Me page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' ,});
+});
+
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Placeholder');
+});
+
+
+
+
+module.exports = router;
